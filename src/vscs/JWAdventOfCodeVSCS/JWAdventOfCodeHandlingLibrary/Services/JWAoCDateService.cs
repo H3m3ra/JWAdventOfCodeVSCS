eebce7ab68bb0f020336f13f6a694f244c813606
Hemera <j576569736572@gmail.com>
2024-12-18 15:28:51 +0100

    Add a static date helper service.

---
+++ src/vscs/JWAdventOfCodeVSCS/JWAdventOfCodeHandlingLibrary/Services/JWAoCDateService.cs
@@ -0,0 +1,25 @@
+﻿namespace JWAdventOfCodeHandlingLibrary.Services;
+
+public static class JWAocDateService
+{
+    public static int? GetShortYearOfFullYear(int year)
+    {
+        var currentFullYear = DateTime.Now.Year;
+        if (year < currentFullYear - 99 || currentFullYear < year)
+        {
+            return null;
+        }
+        return year % 100;
+    }
+
+    public static int ToFullYearFromShortYear(int shortYear)
+    {
+        var fullYear = DateTime.Now.Year;
+        var currentShortYear = fullYear % 100;
+        if (shortYear > currentShortYear)
+        {
+            fullYear -= 100;
+        }
+        return fullYear;
+    }
+}
